7f4936156ae6efe6c5e1c08a47e94b06f73b6ece
Hugo Mancilla <hugo.mancilla@alumnos.uv.cl>
2019-09-10 16:27:54 -0300

    formulario login simple con material design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import HomeComponent from './components/home/HomeComponent';
-import ChannelCreateComponent from './components/channels/ChannelCreateComponent';
-import ClientChannelsComponent from './components/clients/ClientChannelsComponent';
-import ClientInfoComponent from './components/clients/ClientInfoComponent';
 import LoginComponent from './components/login/LoginComponent';
-import Navbar from './components/navbar/NavbarComponent';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <Route path="/" exact component={HomeComponent} />
         <Route path="/login" exact component={LoginComponent} />
-        <Route path="/channelcreate" exact component={ChannelCreateComponent} />
-        <Route path="/channelsclient" exact component={ClientChannelsComponent} />
-        <Route path="/clientinfo" exact component={ClientInfoComponent} />
       </Router>
     </div>
   );
--- src/components/login/LoginComponent.js
+++ src/components/login/LoginComponent.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, FormGroup } from '@material-ui/core';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { makeStyles } from '@material-ui/core/styles';
 import LoginModel from '../../models/LoginModel';
-import LoginService from '../../services/LoginService';
+
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        marginTop: 10,
+        marginBottom: 10
+    },
+}));
 
 function LoginComponent(){
-    return(<div>LoginComponent</div>);
-}
+    const classes = useStyles();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');    
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const login = new LoginModel(null, email, password, null, null, null);
+        console.log(login);
+    };
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <FormGroup className={classes.formControl}>
+                <TextField 
+                    value={email}
+                    onChange={(e) => {setEmail(e.target.value)}}
+                    label="Correo electrónico" 
+                />
+            </FormGroup>
+            <FormGroup className={classes.formControl}>
+                <TextField 
+                    value={password}
+                    onChange={(e) => {setPassword(e.target.value)}}
+                    label="Contraseña"
+                    type="password"
+                />
+            </FormGroup>
+            <Button type="submit" variant="contained" color="primary">
+                Iniciar Sesion
+                <ChevronRightIcon/>    
+            </Button>
+            <a style={{ marginLeft: 10 }} href="google.cl">¿Ha olvidado su contraseña?</a>
+        </form>
+    );
+}
 export default LoginComponent;
